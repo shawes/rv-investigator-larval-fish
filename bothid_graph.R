edf4e491b820c6b7eafea44004af21142577782d
stevehawes <maxhawes@gmail.com>
2015-10-11 17:44:09 +1100

    Added graphs
    Created the graphs for each fish and the fishers exact tests

---
+++ bothid_graph.R
@@ -0,0 +1,46 @@
+Input_Bothids =(
+  "Stage  Depth Count
+  Preflexion   Surface    18
+  Flexion  Surface  10
+  Postflexion    Surface   4
+  Preflexion   Top    17
+  Flexion  Top  12
+  Postflexion    Top   4
+  Preflexion   Bottom    5
+  Flexion  Bottom  8
+  Postflexion    Bottom   6
+  ")
+
+fish_prop <- read.table(textConnection(Input_Bothids),header=TRUE)
+
+fish_prop=
+  mutate(fish_prop,
+         Depth = factor(Depth,levels=c("Surface","Top","Bottom")),
+         Stage = factor(Stage,levels=unique(Stage))
+  )
+fish_prop$ Sum[fish_prop$Stage == 'Preflexion'] =
+  sum(fish_prop$ Count[fish_prop$Stage == 'Preflexion'])
+
+fish_prop$ Sum[fish_prop$Stage == 'Flexion'] =
+  sum(fish_prop$ Count[fish_prop$Stage == 'Flexion'])
+
+fish_prop$ Sum[fish_prop$Stage == 'Postflexion'] =
+  sum(fish_prop$ Count[fish_prop$Stage == 'Postflexion'])
+
+fish_prop=
+  mutate(fish_prop,
+         prop = Count / Sum
+  )
+
+ggplot(fish_prop, 
+       aes(x = Depth, y = prop, fill = Stage, group=Stage, shape=Stage, colour=Stage, ymax=1.0, ymin=0)) +
+  geom_line(size=1.5) +
+  geom_point(size=4) +
+  scale_y_continuous(breaks = seq(0, 1.0, 0.1), 
+                     limits = c(0, 1.0), 
+                     expand = c(0, 0)) +
+  scale_x_discrete(labels=c("0-3","3-50","50-100")) +
+  labs(x = "Depth (m)", y = "Proportion") +
+  scale_colour_brewer(palette="Dark2") +
+  theme(text = element_text(size=20))
+
--- contingency_tables.R
+++ contingency_tables.R
@@ -1,18 +1,48 @@
-flexion_df <- read.csv('flexion_data.csv')
+flexion_df <- read.csv('flexion_data.csv', colClasses = c("factor","factor","character","factor","factor","numeric"))
 
-bothids <- matrix(c(10,12,8, 4,4,6, 18,17,5), 3, 3,dimnames = list(depth = c("Surface", "Top", "Bottom"),
-                                                                   stage = c("Preflexion", "Flexion", "Postflexion")))
-fisher.test(bothids)
+bothids <- matrix(c(18,17,5, 10,12,8, 4,4,6), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
 
-bothids2 <- matrix(c(12,8,4,6,17,5), 2, 3,dimnames = list(depth = c("Top", "Bottom"),
-                                                                   stage = c("Preflexion", "Flexion", "Postflexion")))
-fisher.test(bothids,simulate.p.value = TRUE, B = 1e5)
+labrids <- matrix(c(5,14,2, 38,68,58, 14,101,95), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
 
-labrids <- matrix(c(38,68,61, 14,101,95, 5,14,2), 3, 3,dimnames = list(depth = c("Surface", "Top", "Bottom"),
-                                                                   stage = c("Preflexion", "Flexion", "Postflexion")))
-fisher.test(bothids)
+mullids <- matrix(c(4,0,0, 16,5,0, 26,11,4), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
 
-fisher.multcomp(labrids, 
-                p.method = "none")
+poms <- matrix(c(6,5,1, 5,16,4, 0,4,1), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
 
-test <- acast(flexion_df, Depth~Stage, sum)
+scarids <- matrix(c(1,1,2, 14,20,16, 12,12,15), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
+
+scorps <- matrix(c(7,3,0, 9,15,7, 4,16,19), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
+
+synods <- matrix(c(9,6,4, 7,16,8, 2,8,1), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
+
+serranids <- matrix(c(8,5,6, 4,16,10, 0,4,1), 3, 3,
+                  dimnames = list(depth = c("Surface", "Top", "Bottom"),
+                                  stage = c("Preflexion", "Flexion", "Postflexion")))
+
+
+fisher.test(bothids,alternative="two.sided")
+fisher.test(labrids,alternative="two.sided")
+fisher.test(scorps,alternative="two.sided")
+fisher.test(serranids,alternative="two.sided")
+fisher.test(synods,alternative="two.sided")
+fisher.test(mullids,alternative="two.sided")
+fisher.test(scarids,alternative="two.sided")
+fisher.test(poms,alternative="two.sided")
+
+
+fisher.multcomp(scorps,"bonferroni")
+fisher.multcomp(labrids,"bonferroni")
+
